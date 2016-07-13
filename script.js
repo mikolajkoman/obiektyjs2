ef0772f8606c9c0d6fc779539be46e45fd1f8411
Mike <mikikoman@gamil.com>
2016-07-14 01:33:05 +0200

    latwo poszlo, za latwo :)

---
+++ script.js
@@ -0,0 +1,30 @@
+$(function(){
+	function Button(text) {
+	this.text = text || 'Hello';
+	
+	}
+	Button.prototype = {
+		create: function() {
+			this.$element = $('<button>');
+			this.$element.text(this.text);
+			this.$element.click(function() {
+				alert(self.text);
+			});
+			$('body').append(this.$element);
+		}
+	}
+	var btn1 = new Button('Hello!');
+	var btn2 = new Button('Hello!');
+	var btn3 = new Button('Hello!');
+	var btn4 = new Button('Hello!');
+	var btn5 = new Button('Hello!');
+
+	btn1.create();
+	btn2.create();
+	btn3.create();
+	btn4.create();
+	btn5.create();
+});
+
+
+
