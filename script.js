f117baf77b2e66051ee67bd79a472e4f3474bb01
Mike <mikikoman@gamil.com>
2016-07-14 12:05:25 +0200

    deklaracja self

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(function(){
 	function Button(text) {
 	this.text = text || 'Hello';
-	
 	}
 	Button.prototype = {
+
+
 		create: function() {
+			var self = this;
 			this.$element = $('<button>');
 			this.$element.text(this.text);
 			this.$element.click(function() {
